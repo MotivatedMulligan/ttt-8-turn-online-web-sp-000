2aa7478b629c69e047b8aef542275ef41badf89f
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-14 19:57:02 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,8 +7,8 @@ def display_board(board)
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def move(array, index, character)
-  array[index] = character
+def move(array, index, current_player)
+  array[index] = current_player
 end
 
 def position_taken?(board, index)
